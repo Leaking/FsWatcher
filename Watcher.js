0565129edf3ba748c5f5cbc164fd329545f8dcbb
quinnhzchen <quinnhzchen@tencent.com>
2019-02-19 22:19:38 +0800

    update

--- Watcher.js
+++ Watcher.js
@@ -1,18 +1,23 @@
 const fs = require('fs');
 const watchedDir = __dirname + '/resources'
-console.log('start to watch dir ', watchedDir)
-fs.watch(watchedDir, {recursive: true}, (event, filename) => {
-    const targetFile = watchedDir + '/' + filename
-    if('rename' === event) {
-        const targetFileIsExist = fs.existsSync(targetFile)
-        if(targetFileIsExist) {
-            const isDirectory = fs.statSync(targetFile).isDirectory
-            console.log('Create a', isDirectory ? 'file' : 'directory', targetFile)
-        } else {
-            console.log('Delete file/dir', targetFile)
-        }
-    } else {
-        console.log('Modify file', targetFile)
-    }
-})
 
+function watchDir(directory, callback) {
+    console.log('start to watch dir ', directory)
+    fs.watch(directory, {recursive: true}, (event, filename) => {
+        const targetFile = directory + '/' + filename
+        if('rename' === event) {
+            const targetFileIsExist = fs.existsSync(targetFile)
+            if(targetFileIsExist) {
+                const isDirectory = fs.statSync(targetFile).isDirectory
+                callback('create', targetFile)
+            } else {
+                callback('delete', targetFile)
+            }
+        } else {
+            callback('modify', targetFile)
+        }
+    })
+}
+watchDir(watchedDir, function (event, filename) {
+    console.log(event, filename);
+})
